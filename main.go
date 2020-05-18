e2bf890b2e83c4ee6a66e1de392ddf5d7da248b9
tommady <tommady@users.noreply.github.com>
2020-05-18 15:46:06 +0800

    move out worker pool

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"sync"
 	"time"
 
 	"github.com/line/line-bot-sdk-go/linebot"
@@ -32,44 +31,6 @@ const (
 var (
 	errNoObject = errors.New("no object")
 )
-
-type jobFunc func() error
-
-type workerPool struct {
-	wg   *sync.WaitGroup
-	pool chan jobFunc
-}
-
-func newWorkerPool(poolSize, workerNum int) *workerPool {
-	wp := &workerPool{
-		wg:   new(sync.WaitGroup),
-		pool: make(chan jobFunc, poolSize),
-	}
-
-	for i := 0; i < workerNum; i++ {
-		wp.wg.Add(1)
-		go wp.worker()
-	}
-
-	return wp
-}
-
-func (wp *workerPool) worker() {
-	for job := range wp.pool {
-		if err := job(); err != nil {
-			log.Println(err)
-		}
-	}
-}
-
-func (wp *workerPool) PutJob(job jobFunc) {
-	wp.pool <- job
-}
-
-func (wp *workerPool) Close() {
-	close(wp.pool)
-	wp.wg.Wait()
-}
 
 func main() {
 	var err error
---
+++ wp.go
@@ -0,0 +1,39 @@
+package main
+
+type jobFunc func() error
+
+type workerPool struct {
+	wg   *sync.WaitGroup
+	pool chan jobFunc
+}
+
+func newWorkerPool(poolSize, workerNum int) *workerPool {
+	wp := &workerPool{
+		wg:   new(sync.WaitGroup),
+		pool: make(chan jobFunc, poolSize),
+	}
+
+	for i := 0; i < workerNum; i++ {
+		wp.wg.Add(1)
+		go wp.worker()
+	}
+
+	return wp
+}
+
+func (wp *workerPool) worker() {
+	for job := range wp.pool {
+		if err := job(); err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func (wp *workerPool) PutJob(job jobFunc) {
+	wp.pool <- job
+}
+
+func (wp *workerPool) Close() {
+	close(wp.pool)
+	wp.wg.Wait()
+}
